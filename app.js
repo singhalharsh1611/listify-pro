f95d902d6e528d4a00fc61dbe80df874f3d08d0e
Harsh Singhal <singhalharsh1611@gmail.com>
2024-07-09 11:20:24 +0530

    fixing vercel deployment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = express();
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
